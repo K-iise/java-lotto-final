5f270af01e7594b7ec063a18dddd5799789a8e5e
K-iise <91732817+K-iise@users.noreply.github.com>
2026-01-07 18:47:58 +0900

    feat: 로또 검증 기능 추가

---
+++ src/main/java/lotto/model/Lotto.java
@@ -0,0 +1,40 @@
+package lotto.model;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class Lotto {
+    private final List<Integer> numbers;
+
+    public Lotto(List<Integer> numbers) {
+        validate(numbers);
+        validateRange(numbers);
+        validateDuplication(numbers);
+        this.numbers = numbers;
+    }
+
+    private void validate(List<Integer> numbers) {
+        if (numbers.size() != 6) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
+        }
+    }
+
+    private void validateRange(List<Integer> numbers){
+        for (Integer number : numbers) {
+            if (!(number >= 1 && number <= 45)){
+                throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+            }
+        }
+    }
+
+    private void validateDuplication(List<Integer> numbers) {
+        Set<Integer> fake = new HashSet<>();
+        fake.addAll(numbers);
+        if (fake.size() != 6) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 중복될 수 없습니다.");
+        }
+    }
+
+    // TODO: 추가 기능 구현
+}
